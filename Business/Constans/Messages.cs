f9dd2abab87f877e236923fff81f1d2200e687c7
KADER <KADER@DESKTOP-T0C0UKP>
2021-02-13 02:37:07 +0300

    13 şubat

---
+++ Business/Constans/Messages.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constans
+{
+    public static class Messages
+    {
+        
+        public static string ProductAdded = "ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        internal static string MainintenanceTime = "Sistem bakımda";
+        internal static string ProductsListed= "Ürünler listelendi";
+    }
+}
---
+++ Core/Utilities/DataResult.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities
+{
+    public class DataResult<T> : Result, IDataResult<T>
+    {
+
+        public DataResult(T data,bool succees, string message):base(succees,message)
+        {
+        Data= data;
+        }
+        public DataResult(T data,bool success):base(success)
+        {
+            Data = data;
+        }
+        public T Data { get; }
+    }
+}
